44eccd7a890eb25b9b97424676f3533a8572f662
Kritan-Timilsina <timilsinakritan04@gmail.com>
2026-04-18 20:58:59 +0545

    Removed Commented code of file handling

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ class Student:
         self.semester = semester
         self.byear = byear
 
-# Storing unit-----------------
+
 
 
 studentslist = []
@@ -58,12 +58,6 @@ def addStudent():  # This Function adds student
         conn.close()
         return 
 
-    # for s in studentslist:
-    #     if roll == s.roll:
-    #         print("Student Having this roll number already Exists")
-    #         print(
-    #             "-----------------------------------------------------------------------------")
-    #         return
 
     try:
         name = str(input("Enter your name:"))
@@ -87,11 +81,10 @@ def addStudent():  # This Function adds student
     finally:  
         cur.close()
         conn.close()
-    # s = Student(roll, name, gender, degree, semester, byear)
-    # studentslist.append(s)
+    
     
     print("-"*70)
-    # updatefile()
+    
 
 
 def displayStudent():  # This Displays Student With the help of their roll number.
@@ -121,19 +114,6 @@ def displayStudent():  # This Displays Student With the help of their roll numbe
         if conn:
             conn.close()
     
-    
-    # for s in studentslist:
-    #     if seek == s.roll:
-    #         found = True
-    #         print("Data is Displayed below\n")
-    #         print(
-    #             f"ID:{s.roll}\nName:{s.name}\nGender:{s.gender}\nDate of Birth:{s.byear}\nDegree:{s.degree}\nSemester:{s.semester}\n")
-    #         print(
-    #             "-----------------------------------------------------------------------------")
-    #         break
-    # if not found:
-    #     print("Student does not Exist")
-    #     print("-------------------------------------------------------------------------------")
 
 
 def deleteStudent():  # This function is used to delete student data from database
@@ -161,25 +141,6 @@ def deleteStudent():  # This function is used to delete student data from databa
         if conn:
             conn.close()
         
-    # found = False
-    # try:
-    #     seek = int(input("Enter roll no of Desired Student:"))
-    # except:
-    #     print("Please enter valid input")
-    #     return
-    # for s in studentslist:
-    #     if seek == s.roll:
-    #         found = True
-    #         studentslist.remove(s)
-    #         print("Students Data have been removed Sucessfully.")
-    #         updatefile()
-    #         print(
-    #             "-----------------------------------------------------------------------------")
-    #         break
-    # if found == False:
-    #     print("Student not found!!! ")
-    #     print("---------------------------------------------------------------------------------")
-
 
 def updateStudent():  # This function is used to update student info with help of roll number
     conn=None
@@ -219,36 +180,6 @@ def updateStudent():  # This function is used to update student info with help o
             conn.close()
         
 
-    # found = False
-    # try:
-    #     seek = int(input("Enter roll no of Desired Student:"))
-    # except:
-    #     print("Please enter valid input.")
-    #     return
-    # for s in studentslist:
-    #     if s.roll == seek:
-    #         found = True
-    #         try:
-    #             s.name = input("Enter name to be updated:").strip()
-    #             s.degree = input("Enter degree to be updated:").strip()
-    #             s.gender = input("Enter gender to be upgraded:").strip()
-    #             s.semester = input("Enter semester to be updated:").strip()
-    #             s.byear = input("Enter Date of birth to be updated:")
-    #             print("Students Data updated Sucessfully")
-    #             #updatefile()
-    #             print(
-    #                 "---------------------------------------------------------------------------")
-    #             break
-    #         except:
-    #             print("Please enter valid input.")
-    #             return
-    #         print(
-    #             "-----------------------------------------------------------------------------")
-    # if found == False:
-    #     print("Student not found!!!")
-    #     print("----------------------------------------------------------------------------------")
-
-
 def displayAll():  # This program is used to display all students data
     conn=None
     cur=None
@@ -261,12 +192,12 @@ def displayAll():  # This program is used to display all students data
             print("Table is empty!")
             print("-"*70)
             return 
-        print(f"{'Roll':<6}{'Name':<40}{'Gender':<10}{'Degree':<10}{'Semester':<10}{'Birth Year':<10}")
+        print(f"{'Roll':<6}{'Name':<40}{'Gender':<10}{'Degree':<25}{'Semester':<10}{'Birth Year':<10}")
         print("-" * 100)
 
         print("-"*70)
         for s in allstudents:
-            print(f"{s[0]:<6}{s[1]:<25}{s[2]:<10}{s[3]:<10}{s[4]:<10}{s[5]:<10}")
+            print(f"{s[0]:<6}{s[1]:<40}{s[2]:<10}{s[3]:<25}{s[4]:<10}{s[5]:<10}")
         print("-"*100)
     
     except Exception as e:
@@ -277,49 +208,13 @@ def displayAll():  # This program is used to display all students data
         if conn:
             conn.close()
 
-    # print("------------------Data of Student ----------------------------------------")
-    # if len(studentslist) == 0:
-    #     print("No students in list")
-    #     print("--------------------------------------------------")
-    #     return
-    # for s in studentslist:
-    #     print(f"ID:{s.roll}\nName:{s.name}\nGender:{s.gender}Date of Birth:{s.byear}\nDegree:{s.degree}\nSemester:{s.semester}\n")
-    #     print("-------------------------------------------------------------------------------------")
 
 
-#def updatefile():
-
-    # with open('students.txt', 'w') as f:
-    #     for s in studentslist:
-    #         data = f"{s.roll},{s.name},{s.gender},{s.degree},{s.semester},{s.byear}\n"
-    #         f.write(data)
-
-
-#def loadFromFile():
-    # studentslist.clear()
-    # try:
-    #     with open('students.txt', 'r') as f:
-    #         for line in f:
-
-    #             data = line.strip().split(',')
-    #             if len(data) != 6:
-    #                 continue
-    #             roll = int(data[0])
-    #             name = data[1]
-    #             gender = data[2]
-    #             degree = data[3]
-    #             semester = data[4]
-    #             byear = data[5]
-    #             s = Student(roll, name, gender, degree, semester, byear)
-    #             studentslist.append(s)
-    # except FileNotFoundError:
-    #     print("Loading of file failed")
-    #     return
 
 # -------------------------Main Body------------------------------------
 
 
-#loadFromFile()
+
 while (True):
     print("1. Enter a Student")
     print("2. Display Students Info")
